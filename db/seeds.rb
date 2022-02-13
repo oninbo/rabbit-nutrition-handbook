ce36f84279c7a1ef4c8524c0aed7191ec322e056
oninbo <oninbo@gmail.com>
2022-02-13 20:07:02 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+%i[fruits vegetables ingredient].each do |method|
+  30.times do
+    product = Product.new
+    product.name = Faker::Food.send(method)
+    product.description = Faker::Lorem.paragraph_by_chars(number: 1024)
+    product.save
+  end
+end
